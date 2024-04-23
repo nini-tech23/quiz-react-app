cff34d6135a0044874fc293963a566e5dbc3cfcb
inbo <inbo@cj.net>
2024-04-23 22:04:11 +0800

    Add categories data

---
+++ src/categoryMapping.js
@@ -0,0 +1,25 @@
+const categoryMapping = {
+    "General Knowledge": [
+        { id: 9, name: "General Knowledge" },
+        { id: 21, name: "Sports" },
+        { id: 22, name: "Geography" },
+        { id: 23, name: "History" },
+        { id: 24, name: "Politics" },
+        { id: 25, name: "Art" },
+    ],
+    Entertainment: [
+        { id: 10, name: "Entertainment: Books" },
+        { id: 11, name: "Entertainment: Film" },
+        { id: 12, name: "Entertainment: Music" },
+        { id: 14, name: "Entertainment: Television" },
+        { id: 15, name: "Entertainment: Video Games" },
+        { id: 32, name: "Entertainment: Cartoon & Animations" },
+    ],
+    Science: [
+        { id: 17, name: "Science & Nature" },
+        { id: 18, name: "Science: Computers" },
+        { id: 19, name: "Science: Mathematics" },
+        { id: 30, name: "Science: Gadgets" },
+    ],
+};
+export default categoryMapping;
