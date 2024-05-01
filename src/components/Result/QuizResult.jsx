4fda08a8dafcb14d51b8ed9ef7455829c998f94f
Lkhagvadulam <gg.lkhagvadulam@gmail.com>
2024-05-01 11:45:07 +0800

    Solve incorrect answer showing error & add link to move back to result list

--- src/components/Result/QuizResult.jsx
+++ src/components/Result/QuizResult.jsx
@@ -1,5 +1,6 @@
 import ResultQuestion from "./ResultQuestion";
 import useQuizResultHandler from "../../hooks/useQuizResultHandler";
+import { Link } from "react-router-dom";
 const QuizResult = ({ resultId }) => {
     const { data } = useQuizResultHandler(resultId);
     if (!data || !data.questions) return <div>Loading...</div>;
@@ -7,6 +8,7 @@ const QuizResult = ({ resultId }) => {
     return (
         <>
             <div className="results-container">
+                <Link to='/user'>Back to the result list</Link>
                 <h1 className="result-title">Quiz Result</h1>
                 <div className="result-info">
                     <p>
@@ -26,7 +28,7 @@ const QuizResult = ({ resultId }) => {
                             options={item.options}
                             currentQuestionNumber={index + 1}
                             selectedAnswer={item.userAnswer}
-                            isCorrect={item.correctAnswer}
+                            isCorrect={item.correctAnswer===item.userAnswer}
                         />
                         <p className="correct-answer">
                             <b style={{ fontWeight: "600" }}>Correct Answer: </b>
